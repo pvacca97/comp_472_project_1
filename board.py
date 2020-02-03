20115506ac63b514a515e08b2e5c6927e7286e08
panoreak <panorea.kontis@gmail.com>
2020-02-03 00:01:28 -0500

    Added function that simulates touching a token (chosen token and its 4 adjacent tokens are flipped)

--- board.py
+++ board.py
@@ -9,3 +9,20 @@ def create_board(n, values):
       b[...] = values[i]
       i += 1
   return board
+
+def touch_token(board, row, column):
+  if row > board.shape[0]-1 or column > board.shape[1]-1:
+    raise Exception("Invalid coordinate")
+  flip_token(board, row, column)
+  if row != 0:
+    flip_token(board,row-1,column)
+  if row != board.shape[0]-1:
+    flip_token(board, row+1,column)
+  if column != 0:
+    flip_token(board, row, column-1)
+  if column != board.shape[1] - 1:
+    flip_token(board, row, column+1)
+
+def flip_token(board, row, column):
+  board[row][column] = not board[row][column]
+
--- input.py
+++ input.py
@@ -1,6 +1,6 @@
 import logging as log
 import sys
-from board import create_board
+from board import create_board, touch_token
 
 n = int(sys.argv[1])
 max_d = int(sys.argv[2])
@@ -10,4 +10,7 @@ values = sys.argv[4]
 board = create_board(n, values)
 
 print(board.shape)
+print(board)
+
+touch_token(board,3,3)
 print(board)
