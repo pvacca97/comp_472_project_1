345c02ab7e828ef7cfead1c50e3eef68e2802cfa
pvacca97 <patrick.vacca@hotmail.com>
2020-02-06 17:08:09 -0500

    can now read from input file instead of command line directly

--- input.py
+++ input.py
@@ -4,13 +4,19 @@ from board import create_board, touch_token
 from dfs import dfs
 from graph_node import GraphNode
 
-n = int(sys.argv[1])
-max_d = int(sys.argv[2])
-max_l = int(sys.argv[3])
-values = sys.argv[4]
+file_name = sys.argv[1]
+file = open(file_name, 'r')
 
-board = create_board(n, values)
-print(board.shape)
-print(board)
+for line in file:
+  line.split()
+  n = int(line[0])
+  max_d = int(line[1])
+  max_l = int(line[2])
+  values = line[3]
 
-dfs(board, max_d)
+  board = create_board(n, values)
+  print(board.shape)
+  print(board)
+
+  dfs(board, max_d)
+file.close()
