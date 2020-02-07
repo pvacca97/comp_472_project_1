ec1591cc145b88378ad1795adebd2e8094f4325e
panoreak <panorea.kontis@gmail.com>
2020-02-06 22:53:06 -0500

    Optimized algorithm by using a hash set for comparisons

--- dfs.py
+++ dfs.py
@@ -7,16 +7,17 @@ from graph_node import GraphNode
 
 def dfs(board, max_d):
     root_node = GraphNode(board)
-    open = [root_node]
-    closed = []
+    open_nodes = [root_node] # list of nodes to visit next, in order of priority
+    closed_nodes = [] # list of nodes already visited
+    open_and_closed_hash = set([np.array2string(root_node.state)]) # keeps track of all nodes in open/closed lists
 
     board_size = root_node.state.shape[0]
     goal_state = np.zeros((board_size, board_size), dtype=np.uint8)
 
-    while len(open) != 0:
+    while len(open_nodes) != 0:
 
         # take first node in open list and check for goal state
-        current_node = open.pop(0)
+        current_node = open_nodes.pop(0)
         if np.array_equal(current_node.state, goal_state):
             print("Found solution!")
             print("Solution path:")
@@ -25,27 +26,30 @@ def dfs(board, max_d):
             while current_solution_node is not None:
                 solution_path.insert(0, current_solution_node)
                 current_solution_node = current_solution_node.parent
+
+            # TODO The following lines print the solution path. Move to solution output file.
             for i in range(len(solution_path)):
                 print(solution_path[i].get_action_and_state())
             return
 
-        # Only add children of current node if the max depth is not reached
+        # generate children of current node if the max depth is not reached
         elif current_node.depth != max_d:
             child_nodes = []
-            # generate children of current node, discard ones with state equal to any node in closed or open list
+            # generate children of current node
             for i, j in itertools.product(range(board_size), range(board_size)):
                 child_state = touch_token(current_node.state, i, j)
-                if not any(np.array_equal(node.state, child_state) for node in closed) and \
-                        not any(np.array_equal(node.state, child_state) for node in open):
+
+                # discard children that have states already in the open or closed lists (all generated nodes)
+                if np.array2string(child_state) not in open_and_closed_hash:
                     child_nodes.append(GraphNode(child_state, current_node, (i, j)))
+                    open_and_closed_hash.add(np.array2string(child_state))
 
             # order child nodes by position of first white token
             child_nodes.sort(key=lambda node: node.first_white_token_position)
 
             # put every node in list of ordered child nodes at the front of open list
-            open = child_nodes + open
+            open_nodes = child_nodes + open_nodes
 
-        # put X on closed
-        closed.append(current_node)
-
+        # put node on closed list
+        closed_nodes.append(current_node)
     print("No solution found")
--- input.py
+++ input.py
@@ -8,15 +8,15 @@ file_name = sys.argv[1]
 file = open(file_name, 'r')
 
 for line in file:
-  line = line.split()
-  n = int(line[0])
-  max_d = int(line[1])
-  max_l = int(line[2])
-  values = line[3]
+    line = line.split()
+    n = int(line[0])
+    max_d = int(line[1])
+    max_l = int(line[2])
+    values = line[3]
 
-  board = create_board(n, values)
-  print(board.shape)
-  print(board)
+    board = create_board(n, values)
+    print(board.shape)
+    print(board)
 
   dfs(board, max_d)
 file.close()
