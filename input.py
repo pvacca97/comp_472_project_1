cb5a908cb212d3b33a46676d7170a25b1e743ac1
pvacca97 <patrick.vacca@hotmail.com>
2020-02-08 17:09:00 -0500

    added two lines to close output files

--- input.py
+++ input.py
@@ -4,15 +4,16 @@ from board import create_board, touch_token
 from dfs import dfs
 
 file_name = sys.argv[1]
-file = open(file_name, 'r')
+input_file = open(file_name, 'r')
 num_lines_in_file = 0
 
-for line in file:
+for line in input_file:
     line = line.split()
     n = int(line[0])
     max_d = int(line[1])
     max_l = int(line[2])
     values = line[3]
+
     solution_file = open(str(num_lines_in_file) + "_dfs_solution.txt", "w")
     search_file = open(str(num_lines_in_file) + "_dfs_search.txt", "w")
     num_lines_in_file += 1
@@ -27,11 +28,14 @@ for line in file:
         solution_file.write('no solution')
     else:
         for i in range(len(solution_path) - 1):
-            solution_file.write(solution_path[i].get_solution_file_line() + '\n')
+            solution_file.write(
+                solution_path[i].get_solution_file_line() + '\n')
         solution_file.write(solution_path[-1].get_solution_file_line())
+    solution_file.close()
 
     for i in range(len(searched_nodes) - 1):
         search_file.write(searched_nodes[i].get_search_file_line() + '\n')
     search_file.write(searched_nodes[-1].get_search_file_line())
+    search_file.close()
 
-file.close()
+input_file.close()
