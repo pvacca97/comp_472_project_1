be4c1b6ce82aae298f230782412ce7ce9f4a14a8
riyaGit <riya.dutta1001@gmail.com>
2020-02-07 21:43:31 -0500

    The solution is not printed in the solution file. The readme is updated.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # comp_472
+
+**To Run:**
+
+Create an input file called `r.txt` with input commands such as `3 8 9 111001011`
+
+From the root directory run `python3 input.py r`
--- input.py
+++ input.py
@@ -1,13 +1,11 @@
-import logging as log
 import sys
-import time
 
 from board import create_board, touch_token
 from dfs import dfs
-from graph_node import GraphNode
 
 file_name = sys.argv[1]
 file = open(file_name, 'r')
+num_lines_in_file = 0
 
 for line in file:
     line = line.split()
@@ -15,6 +13,8 @@ for line in file:
     max_d = int(line[1])
     max_l = int(line[2])
     values = line[3]
+    solution_file = open(str(num_lines_in_file) + "_dfs_solution.txt", "w")
+    num_lines_in_file += 1
 
     board = create_board(n, values)
     print(board.shape)
@@ -24,13 +24,13 @@ for line in file:
     searched_nodes = results[0]
     solution_path = results[1]
 
-    # TODO Make solution output file, and move to solution output file.
     if len(solution_path) == 0:
-        print('no solution')
+        solution_file.write('no solution')
     else:
         for i in range(len(solution_path)):
-            print(solution_path[i].get_action_and_state())
+            solution_file.write(solution_path[i].get_action_and_state() + '\n')
 
-    # TODO make search output file
+        # TODO make search output file
 
 file.close()
+
